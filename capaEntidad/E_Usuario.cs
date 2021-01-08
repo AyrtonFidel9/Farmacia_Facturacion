72ea6b460249c19bb425c5092b0fc0b77b15aba4
Edwin9927 <edwinmanzanoquinzo@gmail.com>
2021-01-07 19:38:56 -0500

    Agregar archivos de proyecto.

---
+++ capaEntidad/E_Usuario.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace capaEntidad
+{
+    class E_Usuario
+    {
+        public string idUsuario { get; set; }
+        public string Clave { get; set; }
+        public string Rol { get; set; }
+    }
+}
