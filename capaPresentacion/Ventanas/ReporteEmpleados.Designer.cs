38ce2e637992a7f3b4360d500bf195bba004d639
Ayrton Avalos <ayrtonfidel9@gmail.com>
2021-01-07 19:48:18 -0500

    Fusionado carpetas de otra solucion Se ha llevado las carpetas Imagenes, Tipos de Letras, Reportes y Ventanas

---
+++ capaPresentacion/Ventanas/ReporteEmpleados.Designer.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace View.Ventanas
+{
+    partial class ReporteEmpleados
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgEmpleados = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dgEmpleados)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgEmpleados
+            // 
+            this.dgEmpleados.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgEmpleados.Location = new System.Drawing.Point(45, 91);
+            this.dgEmpleados.Name = "dgEmpleados";
+            this.dgEmpleados.Size = new System.Drawing.Size(637, 348);
+            this.dgEmpleados.TabIndex = 0;
+            this.dgEmpleados.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // ReporteEmpleados
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(784, 503);
+            this.Controls.Add(this.dgEmpleados);
+            this.Name = "ReporteEmpleados";
+            this.Text = "Reporte";
+            ((System.ComponentModel.ISupportInitialize)(this.dgEmpleados)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+
+        public System.Windows.Forms.DataGridView dgEmpleados;
+    }
+}
